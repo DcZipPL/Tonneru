fe2530ee0bda49970e0ebf018a0773af0c68886a
Kordian Kisieliński <dczip89@gmail.com>
2024-04-21 16:20:16 +0200

    Add Hyperlink control

---
+++ Tonneru/Views/Controls/Hyperlink.axaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Reactive;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using ReactiveUI;
+
+namespace Tonneru.Views.Controls;
+
+public class Hyperlink : Button
+{
+	public Hyperlink()
+	{
+		Click += (_, _) => LinkClick();
+	}
+
+	public static readonly StyledProperty<string> LinkProperty = AvaloniaProperty.Register<Hyperlink, string>(nameof(Link));
+
+	public string Link
+	{
+		get => GetValue(LinkProperty);
+		set => SetValue(LinkProperty, value);
+	}
+
+	public void LinkClick()
+	{
+		Process.Start(new ProcessStartInfo(Link) { UseShellExecute = true });
+	}
+}
