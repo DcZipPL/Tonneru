ed24274f583aadb4179b8d359589ba0b96634720
Kordian Kisieliński <dczip89@gmail.com>
2024-04-21 15:03:17 +0200

    Implement IconButton templated control

---
+++ Tonneru/Views/Controls/IconButton.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Tonneru.Views.Controls;
+
+public class IconButton : Button
+{
+	public static readonly StyledProperty<string> IconProperty =
+		AvaloniaProperty.Register<IconButton, string>(nameof(Icon), defaultValue: "&#xE038;");
+	public string Icon
+	{
+		get => GetValue(IconProperty);
+		set => SetValue(IconProperty, value);
+	}
+}
